0d282cf6f1a39832810be2d8005237447fc06d8a
Zahmadgit <112338853+Zahmadgit@users.noreply.github.com>
2023-08-28 19:39:59 -0400

    obtaining data through requests.get while providing headers

--- reminder.py
+++ reminder.py
@@ -1,1 +1,13 @@
 import requests
+#location of all the assignments
+url = "https://courses.ianapplebaum.com/syllabus/4"
+
+headers = {
+    "Authorization": "Bearer {Sl9TiE5yh4JAKIJVKl7V7FpafWf03AmoSrcT2SeW}",
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+response = requests.get(url, headers=headers).json()
+
+print(response)
