39482ec095a20c88604048e54332ff170a8e236f
Carla Delima <del.cj126@gmail.com>
2023-08-29 10:10:56 -0400

    Capable of sending emails from Python

--- email.py
+++ email.py
@@ -1,3 +1,23 @@
 import os
+import ssl
+import smtplib
 from email.message import EmailMessage
 
+sender = 'encouragingreminders2023@gmail.com'
+password = 'wbuhfzguowizlode'
+receiver = 'tuk87239@temple.edu'
+
+subject_line = 'Hey! Just wanted to remind you that...'
+body = '' #something generated here
+
+email = EmailMessage()
+email['From'] = sender
+email['To'] = receiver
+email['Subject'] = subject_line
+email.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(sender, password)
+    smtp.sendmail(sender, receiver, email.as_string())
